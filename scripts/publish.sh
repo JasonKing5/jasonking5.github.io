7c1dd6a90779aad7438824c27ef6a8e98a78b7b6
Jason <wangsanmu5@163.com>
2025-12-18 11:36:27 +0800

    feat: use pnpm

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,34 +1,25 @@
 #!/bin/bash
 
 # 拉取指定git仓库的代码
-# git clone https://github.com/JasonKing5/jasonking5.github.io.git
+# git clone https://github.com/JasonKing5/blog.git
 
 # 进入代码目录
-echo "cd /var/repo/jasonking5.github.io"
-cd /var/repo/jasonking5.github.io
+echo "cd /home/ubuntu/repo/blog"
+cd /home/ubuntu/repo/blog
 
 echo "current pwd:"
 pwd
 
 # 更新代码
-echo "sudo git pull"
-sudo git pull
-
-# 进入网站代码根目录
-# echo "cd ./website"
-# cd ./website
-# pwd
+echo "git pull"
+git pull
 
 # 执行 npm install 并等待完成，然后执行 npm run build
-echo "sudo npm install && npm run build"
-sudo npm run build
-
-# remove /build/images folder
-echo "rm -rf /var/repo/jasonking5.github.io/build/images"
-sudo rm -rf /var/repo/jasonking5.github.io/build/images
+echo "pnpm install && pnpm run build"
+pnpm install && pnpm run build
 
 # 复制构建后的文件到目标目录并强制替换覆盖
-echo "sync website"
-sudo rsync -av --delete /var/repo/jasonking5.github.io/build/ /var/www/codefe.cn/
+echo "sync build files to /var/www/codefe.cn"
+sudo rsync -av --delete /home/ubuntu/repo/blog/build/ /var/www/codefe.cn/
 
 echo "[SUCCESS] deploy"
